a22640798f9c2e79442217dba95581e015e02648
Juan Camilo Puentes PC <jcpsandoval94@gmail.com>
2022-06-13 15:34:55 -0500

    Se  crean datos iniciales para llenar las tablas creadas del sistema

--- TareasContext.cs
+++ TareasContext.cs
@@ -12,13 +12,46 @@ public class TareasContext: DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        List<Categoria> categoriasInit = new List<Categoria>();
+        categoriasInit.Add(new Categoria() {
+            CategoriaId = Guid.Parse("6067e125-9876-4da4-9c73-5011b00dd3cf"),
+            Nombre = "Actividades pendientes",
+            Peso = 20
+            });
+
+        categoriasInit.Add(new Categoria() {
+            CategoriaId = Guid.Parse("6067e125-9876-4da4-9c73-5011b00dd330"),
+            Nombre = "Actividades Personales",
+            Peso = 40
+            });
+
+
         modelBuilder.Entity<Categoria> (categoria =>{
             categoria.ToTable("Categoria");
             categoria.HasKey(p => p.CategoriaId);
             categoria.Property(p => p.Nombre).IsRequired().HasMaxLength(150);
             categoria.Property(p => p.Descripcion);
             categoria.Property(p => p.Peso);
+
+            categoria.HasData(categoriasInit)
         });
+
+        List<Tarea> tareasInit = new List<Tarea>();
+        tareasInit.Add(new Tarea() {
+            TareaId = Guid.Parse("6067e125-9876-4da4-9c73-5011b00dd3ad"),
+            CategoriaId = Guid.Parse("6067e125-9876-4da4-9c73-5011b00dd3cf"),
+            PrioridadTarea = Prioridad.Media,
+            Titulo = "Pago de servicios publicos",
+            FechaCreacion = DateTime.Now
+            });
+
+        tareasInit.Add(new Tarea() {
+            TareaId = Guid.Parse("6067e125-9876-4da4-9c73-5011b00dd312"),
+            CategoriaId = Guid.Parse("6067e125-9876-4da4-9c73-5011b00dd330"),
+            PrioridadTarea = Prioridad.Baja,
+            Titulo = "Terminar de ver pelicula en netflix",
+            FechaCreacion = DateTime.Now
+            });
 
         modelBuilder.Entity<Tarea> (tarea  => {
             tarea.ToTable("Tarea");
@@ -29,6 +62,8 @@ public class TareasContext: DbContext
             tarea.Property(p => p.PrioridadTarea);
             tarea.Property(p => p.FechaCreacion);
             tarea.Ignore(p => p.Resumen);
+
+            tarea.HasData(tareasInit);
 
         });
     }
