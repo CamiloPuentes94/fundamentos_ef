5a54a3a7936f75e00b973332fb81f4ce1aeed544
Juan Camilo Puentes PC <jcpsandoval94@gmail.com>
2022-06-13 10:39:45 -0500

    Se realiza poner la configuracion defluent Api para mejorar sintaxis de entity framework

--- models/Categoria.cs
+++ models/Categoria.cs
@@ -4,12 +4,15 @@ namespace projectef.models;
 
 public class Categoria
 {
-    [Key] // DATA NOTION O ATRIBUTOS
+    //[Key] // DATA NOTION O ATRIBUTOS
     public Guid CategoriaId {get;set;}
 
-    [Required]
-    [MaxLength(150)]
+    // [Required]
+    // [MaxLength(150)]
     public string Nombre {get;set;}
+
     public string Descripcion {get;set;}
+
+    public virtual ICollection<Tarea> Tareas {get;set;} 
 
 }
