9d24e79a10f98dcb07e5b5d64a6fa3c0964ed0a4
Juan Camilo Puentes PC <jcpsandoval94@gmail.com>
2022-06-13 08:35:54 -0500

    Se realizan el mapeo del modelo tareas

--- models/Tareas.cs
+++ models/Tareas.cs
@@ -1,17 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace projectef.models;
 
 public class Tarea
 {
+    [Key] // Attributos
     public Guid TareaId {get;set;}
+
+    [ForeignKey("CategoriaId")] // atributo llave foranea
     public Guid CategoriaId {get;set;}
+
+    [Required]
+    [MaxLength(200)]
     public string Titulo {get;set;}
 
     public string Descripcion {get;set;}
 
     public Prioridad PrioridadTarea {get;set;}
+
     public DateTime FechaCreacion {get;set;}
+
     public virtual Categoria Categoria {get;set;}
-    public virtual ICollection<Tarea> Tareas {get;set;}
+
+    [NotMapped] // con este atributo no carga el mapeo para la base de datos
+    public string Resumen {get;set;}
 }
 
 public enum Prioridad
