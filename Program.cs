be54f40d68688cda6bd4368c747964abde48a9cb
Juan Camilo Puentes PC <jcpsandoval94@gmail.com>
2022-06-15 11:31:22 -0500

    Se agregan datos en la base de datos con el metodo post

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using projectef;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
+using projectef.models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,13 +19,25 @@ app.MapGet("/dbconexion",async([FromServices] TareasContext dbContext) =>
     return Results.Ok("Basede datos en memoria: " + dbContext.Database.IsInMemory());
 });
 
-app.MapGet("/api/tareas", async ([FromServices] TareasContext dbContext)=>
+app.MapGet("/api/tareas", async ([FromServices] TareasContext dbContext) =>
 {
     return Results.Ok(dbContext.Tareas.Include(p => p.Categoria).Where(p => p.PrioridadTarea == projectef.models.Prioridad.Baja));
 });
 
-app.MapGet("/api/tareas2", async ([FromServices] TareasContext dbContext)=>
+app.MapGet("/api/tareas2", async ([FromServices] TareasContext dbContext) =>
 {
-    return Results.Ok(dbContext.Tareas.Include(p => p.Categoria).Where(p => p.PrioridadTarea == projectef.models.Prioridad.Media));
+    return Results.Ok(dbContext.Tareas.Include(p => p.Categoria));
 });
+
+app.MapPost("/api/tareas2", async ([FromServices] TareasContext dbContext, [FromBody] Tarea tarea) =>
+{
+    tarea.TareaId = Guid.NewGuid();
+    tarea.FechaCreacion = DateTime.Now;
+    await dbContext.AddAsync(tarea); //De esta manera se puede agregar un registro
+
+    await dbContext.SaveChangesAsync();
+
+    return Results.Ok();
+});
+
 app.Run();
