2293b41779d85818aa31e34b87ca293af8d5caa6
Juan Camilo Puentes PC <jcpsandoval94@gmail.com>
2022-06-15 12:01:56 -0500

    Se realizan consultas a solo la tabla categorias y se hace cambio para actualizar las categorias

--- Program.cs
+++ Program.cs
@@ -13,11 +13,15 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+// hacer llamado de datos de la base de datos
+
 app.MapGet("/dbconexion",async([FromServices] TareasContext dbContext) =>
 {
     dbContext.Database.EnsureCreated();
     return Results.Ok("Basede datos en memoria: " + dbContext.Database.IsInMemory());
 });
+
+// hacer llamado de datos de la tabla tareas
 
 app.MapGet("/api/tareas", async ([FromServices] TareasContext dbContext) =>
 {
@@ -29,6 +33,15 @@ app.MapGet("/api/tareas2", async ([FromServices] TareasContext dbContext) =>
     return Results.Ok(dbContext.Tareas.Include(p => p.Categoria));
 });
 
+// hacer llamado de datos de la tabla categoria
+
+app.MapGet("/api/categoria", async ([FromServices] TareasContext dbContext) =>
+{
+    return Results.Ok(dbContext.Categorias);
+});
+
+//Agregar Registros
+
 app.MapPost("/api/tareas2", async ([FromServices] TareasContext dbContext, [FromBody] Tarea tarea) =>
 {
     tarea.TareaId = Guid.NewGuid();
@@ -38,6 +51,47 @@ app.MapPost("/api/tareas2", async ([FromServices] TareasContext dbContext, [From
     await dbContext.SaveChangesAsync();
 
     return Results.Ok();
+});
+
+// actualizar datos
+
+
+app.MapPut("/api/tareas2/{id}", async ([FromServices] TareasContext dbContext, [FromBody] Tarea tarea, [FromRoute] Guid id) =>
+{
+    var tareaActual = dbContext.Tareas.Find(id); // variable para buscar por id
+
+    if (tareaActual != null)
+    {
+        tareaActual.CategoriaId = tarea.CategoriaId;
+        tareaActual.Titulo = tarea.Titulo;
+        tareaActual.PrioridadTarea = tarea.PrioridadTarea;
+        tareaActual.Descripcion = tarea.Descripcion;
+
+        await dbContext.SaveChangesAsync();
+
+        return Results.Ok();
+    }
+
+    return Results.NotFound();
+});
+
+app.MapPut("/api/categoria/{id}", async ([FromServices] TareasContext dbContext, [FromBody] Categoria categoria, [FromRoute] Guid id) =>
+{
+    var categoriaActual = dbContext.Categorias.Find(id);
+
+    if (categoriaActual != null)
+    {
+        categoriaActual.Nombre = categoria.Nombre;
+        categoriaActual.Descripcion = categoria.Descripcion;
+        categoriaActual.Peso = categoria.Peso;
+        
+
+        await dbContext.SaveChangesAsync();
+
+        return Results.Ok();
+    }
+
+    return Results.NotFound();
 });
 
 app.Run();
